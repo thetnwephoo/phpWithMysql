ac1ceb7e4c99194a2e683f70676c73cd67019438
thetphoongon <thetphoongon456@gmail.com>
2020-02-13 15:25:02 +0630

    all file added

---
+++ catalog.php
@@ -0,0 +1,106 @@
+<?php 
+include("inc/functions.php");
+
+$pageTitle = "Full Catalog";
+$section = null;
+// for initial variable
+$items_per_page = 8;
+
+if (isset($_GET["cat"])) {
+    if ($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+        $section = "books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+        $section = "movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+        $section = "music";
+    }
+}
+
+// for current page
+if(isset($_GET["pg"])) {
+    $current_page = filter_input(INPUT_GET,"pg",FILTER_SANITIZE_NUMBER_INT);
+    // $current_page = $_GET["pg"];
+    // var_dump($current_page);
+    // exit;
+    
+}
+// for empty page
+if(empty($current_page)) {
+    $current_page =1;
+}
+
+$total_items = get_catalog_count($section);
+$total_pages = ceil($total_items / $items_per_page);
+$limit_results = "";
+if (!empty($section)) {
+    $limit_results = "cat=" . $section . "&";
+}
+
+
+//redirect too-large page numbers to the last page
+if($current_page > $total_pages) {
+    header("location:catalog.php?"
+            . $limit_results
+            . "pg=". $total_pages);
+}
+
+//redirect too-small page numbers to the first page
+if ($current_page < 1) {
+    header("location:catalog.php?"
+            . $limit_results
+            .  "pg=1");
+}
+//determine the offset(number of items to skip) for the current page
+// for example: on page 3 with 8 item per page, the offset would be 16
+$offset = ($current_page - 1) * $items_per_page;
+
+if(empty($section)) {
+    $catalog = full_catalog_array($items_per_page,$offset);
+} else {
+    $catalog = category_catalog_array($section,$items_per_page,$offset );
+}
+
+
+
+include("inc/header.php"); ?>
+
+<div class="section catalog page">
+    
+    <div class="wrapper">
+        
+        <h1><?php 
+        if ($section != null) {
+            echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
+        }
+        echo $pageTitle; ?></h1>
+
+        $pagination = "<div class = \"paginatioon\">";
+                $pagination .= "Pages: ";
+                for ($i = 1;$i <= $total_pages;$i++){
+                    if($i == $current_page) {
+                    $pagination .= " <span>$i</span>";
+                } else {
+                    $pagination .= " <a href='catalog.php?";
+                    if(!empty($section)){
+                        $pagination .= "cat=".$section."&";
+                    }
+                    $pagination .= "pg=$i'>$i</a>";
+                    }
+                }
+                    $pagination .= "</div>";
+        
+        <ul class="items">
+            <?php
+            foreach ($catalog as $item) {
+                echo get_item_html($item);
+            }
+            ?>
+        </ul>
+        
+    </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
